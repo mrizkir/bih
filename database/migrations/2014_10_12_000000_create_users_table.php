ecd7415e9b944bf88cfcc516d3ca3502848ca9ee
Mochammad Rizki Romdoni <mr.romdoni@gmail.com>
2023-05-26 14:50:24 +0700

    struktur table user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique();
             $table->string('password');
+            $table->string('name');
+            $table->string('nomor_hp')->unique();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable(); 
+            $table->string('theme')->default('default');
+            $table->string('foto')->default('storage/images/users/no_photo.png');
+            $table->boolean('active')->default(true);
+			$table->boolean('isdeleted')->default(true);
+			$table->string('default_role',15);           
             $table->rememberToken();
             $table->timestamps();
         });
